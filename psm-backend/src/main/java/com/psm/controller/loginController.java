5dc7748411cd3f37b0933906d6dbb565b9baf38d
lasertsole <56973455+lasertsole@users.noreply.github.com>
2023-04-02 00:34:16 +0800

    springboot添加配置使其能产生jar包

--- psm-backend/src/main/java/com/psm/controller/loginController.java
+++ psm-backend/src/main/java/com/psm/controller/loginController.java
@@ -1,13 +1,17 @@
 package com.psm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class loginController {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @Autowired
+    private Environment env;
+    @RequestMapping(value = "/version", method = RequestMethod.GET)
     public String hello(String name){
-        return "hello"+name;
+        return "喵剪辑后端"+env.getProperty("version");
     }
 }
